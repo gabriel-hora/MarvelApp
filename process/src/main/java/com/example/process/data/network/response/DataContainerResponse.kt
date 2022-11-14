c4e8eea1f4a3afbc8aa2ac8ad2821ec7562b7ab6
Gabriel-Hora <gabrielhoradeveloper@gmail.com>
2022-11-13 21:18:16 -0300

    Mapeamento dos objetos de response da api da marvel

---
+++ process/src/main/java/com/example/process/data/network/response/DataContainerResponse.kt
@@ -0,0 +1,5 @@
+package com.example.process.data.network.response
+
+data class DataContainerResponse(
+    val results: List<CharacterResponse>
+)
---
+++ process/src/main/java/com/example/process/data/network/response/ThumbnailResponse.kt
@@ -0,0 +1,6 @@
+package com.example.process.data.network.response
+
+data class ThumbnailResponse(
+    val path: String,
+    val extension: String
+)
